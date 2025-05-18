f313d007465d34ab6a2b87d5321dfea963c2edf2
Reindhart <japp.banjo@gmail.com>
2025-05-17 19:33:59 -0600

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <Reportes />
+  </div>
+</template>
+
+<script>
+import Reportes from './components/Reportes.vue'
+
+export default {
+  components: { Reportes }
+}
+</script>
